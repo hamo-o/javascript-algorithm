112ce6239c9d3eaf2a3185fd1d55d730117382b5
hamo-o <yheel369@gmail.com>
2024-01-13 19:05:22 +0900

    BOJ/11723

--- BOJ/11723/app.js
+++ BOJ/11723/app.js
@@ -3,31 +3,30 @@ const filePath = process.platform === 'linux' ? '/dev/stdin' : './input.txt';
 const input = fs.readFileSync(filePath).toString().trim().split('\n');
 const num = +input[0];
 const commands = input.slice(1).map((line) => line.split(' '));
+let bits = 0;
 
-const s = new Set();
 commands.forEach((command) => {
-  const [com, num] = command;
+  const com = command[0];
+  const num = command.length === 2 ? command[1] : null;
+
   switch (com) {
     case 'add':
-      s.add(num);
+      bits = bits | (1 << num);
       break;
     case 'remove':
-      s.delete(num);
+      bits = bits & ~(1 << num);
       break;
     case 'check':
-      s.has(num) ? console.log(1) : console.log(0);
+      bits & (1 << num) ? console.log(1) : console.log(0);
       break;
     case 'toggle':
-      s.has(num) ? s.delete(num) : s.add(num);
+      bits = bits ^ (1 << num);
       break;
     case 'all':
-      s.clear();
-      for (let i = 1; i < 21; i++) {
-        s.add(i.toString());
-      }
+      bits = bits | ((1 << 21) - 1);
       break;
     case 'empty':
-      s.clear();
+      bits = 0;
       break;
   }
 });
