9d17d99c0196196e9f4af80647851893435a39a6
hamo-o <yheel369@gmail.com>
2024-02-08 05:21:39 +0900

    BOJ/1327

--- BOJ/1327/app.js
+++ BOJ/1327/app.js
@@ -5,37 +5,37 @@ const input = fs.readFileSync(filePath).toString().trim().split('\n');
 const [n, k] = input[0].split(' ').map(Number);
 const numbers = input[1].split(' ').map(Number);
 
-const visited = new Array(n).fill(0);
+const answer = [...numbers].sort((a, b) => a - b).join('');
+const visited = {};
 
-const checkIsUp = (arr) => {
-  for (let i = 0; i < arr.length - 1; i++) {
-    if (arr[i] > arr[i + 1]) {
-      return 0;
+const bfs = (start) => {
+  const queue = [];
+  queue.push(start);
+  visited[start[1]] = 1;
+
+  let front = 0;
+  while (front < queue.length) {
+    const cur = queue[front];
+    const [count, str] = cur;
+    front++;
+
+    if (answer === str) {
+      return count;
     }
-  }
-  return 1;
-};
 
-const dfs = (arr, count) => {
-  console.log(arr);
-  if (checkIsUp(arr)) {
-    return count;
-  }
-
-  for (let i = 0; i < n - k + 1; i++) {
-    console.log(i);
-    if (!visited[i]) {
+    for (let i = 0; i < n - k + 1; i++) {
+      const arr = str.split('');
       for (let j = 0; j < Math.floor(k / 2); j++) {
         [arr[i + j], arr[i + k - 1 - j]] = [arr[i + k - 1 - j], arr[i + j]];
       }
-      visited[i] = 1;
-      dfs(arr, count + 1);
-      visited[i] = 0;
+      const swap = arr.join('');
+      if (!visited[swap]) {
+        visited[swap] = 1;
+        queue.push([count + 1, swap]);
+      }
     }
   }
-
   return -1;
 };
 
-const answer = dfs(numbers, 0);
-console.log(answer);
+console.log(bfs([0, numbers.join('')]));
