aac5648d3e1a38e8467d867d75e1e84bbd7f7e85
hamo-o <yheel369@gmail.com>
2024-02-23 00:32:04 +0900

    BOJ/5582

--- BOJ/5582/app.js
+++ BOJ/5582/app.js
@@ -2,19 +2,48 @@ const fs = require('fs');
 const filePath = process.platform === 'linux' ? '/dev/stdin' : './input.txt';
 
 const input = fs.readFileSync(filePath).toString().trim().split('\n');
-const [a, b] = input;
-const memo = new Array(a.length + 1)
+const [n, m] = input[0].split(' ').map(Number);
+const map = input.slice(1).map((line) => line.split('').map(Number));
+const visited = new Array(n)
   .fill(0)
-  .map(() => new Array(b.length + 1).fill(0));
+  .map(() => new Array(m).fill(0).map(() => new Array(2).fill(0)));
+
+const queue = [];
+visited[0][0][0] = 1;
+queue.push('0 0 0');
+
+let front = 0;
+while (front < queue.length) {
+  const [x, y, count] = queue[front].split(' ').map(Number);
+  front++;
+
+  [
+    [-1, 0],
+    [1, 0],
+    [0, -1],
+    [0, 1],
+  ].forEach(([dx, dy]) => {
+    if (x + dx >= 0 && x + dx < n && y + dy >= 0 && y + dy < m) {
+      if (map[x + dx][y + dy] && !count && !visited[x + dx][y + dy][1]) {
+        visited[x + dx][y + dy][1] = visited[x][y][0] + 1;
+        queue.push(`${x + dx} ${y + dy} 1`);
+      } else if (!map[x + dx][y + dy] && !visited[x + dx][y + dy][count]) {
+        visited[x + dx][y + dy][count] = visited[x][y][count] + 1;
+        queue.push(`${x + dx} ${y + dy} ${count}`);
+      }
+    }
+  });
+}
 
 let answer = 0;
-for (let i = 1; i <= a.length; i++) {
-  for (let j = 1; j <= b.length; j++) {
-    if (a[i - 1] === b[j - 1]) {
-      memo[i][j] = memo[i - 1][j - 1] + 1;
-      answer = Math.max(answer, memo[i][j]);
-    }
-  }
+if (visited[n - 1][m - 1][0] && visited[n - 1][m - 1][1]) {
+  answer = Math.min(visited[n - 1][m - 1][0], visited[n - 1][m - 1][1]);
+} else if (visited[n - 1][m - 1][0]) {
+  answer = visited[n - 1][m - 1][0];
+} else if (visited[n - 1][m - 1][1]) {
+  answer = visited[n - 1][m - 1][1];
+} else {
+  answer = -1;
 }
 
 console.log(answer);
