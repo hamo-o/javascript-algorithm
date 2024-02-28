f51972909b56d4562155878b8092c84449f3ca0b
hamo-o <yheel369@gmail.com>
2024-02-28 15:17:48 +0900

    BOJ/14002

--- BOJ/14002/app.js
+++ BOJ/14002/app.js
@@ -4,41 +4,34 @@ const filePath = process.platform === 'linux' ? '/dev/stdin' : './input.txt';
 const input = fs.readFileSync(filePath).toString().trim().split('\n');
 const n = +input[0];
 const numbers = input[1].split(' ').map(Number);
-const arr = [numbers[0]];
-const memo = new Array(n).fill(0);
+const memo = new Array(n).fill(0).map(() => []);
 
-for (let i = 1; i < n; i++) {
-  let left = 0;
-  let right = arr.length - 1;
-  if (arr[right] < numbers[i]) {
-    arr.push(numbers[i]);
-    memo[i] = arr.length - 1;
-  } else {
-    while (left <= right) {
-      const mid = Math.floor((left + right) / 2);
+for (let i = 0; i < n; i++) {
+  memo[i].push(numbers[i]);
+}
 
-      if (left === right || arr[mid] === numbers[i]) {
-        memo[i] = mid;
-        arr[mid] = numbers[i];
-        break;
-      }
-
-      if (arr[mid] < numbers[i]) {
-        left = mid + 1;
-      } else {
-        right = mid;
+for (let i = 0; i < n; i++) {
+  const pivot = numbers[i];
+  for (let j = i + 1; j < n; j++) {
+    if (pivot < numbers[j]) {
+      const a = memo[i].length;
+      const b = memo[j].length;
+      if (a + 1 > b) {
+        memo[j] = memo[i].slice();
+        memo[j].push(numbers[j]);
       }
     }
   }
 }
 
-let idx = arr.length - 1;
-for (let i = n - 1; i >= 0; i--) {
-  if (memo[i] === idx) {
-    arr[idx] = numbers[i];
-    idx--;
+let len = 0;
+let answer = '';
+memo.forEach((arr) => {
+  if (len < arr.length) {
+    len = arr.length;
+    answer = arr.join(' ');
   }
-}
+});
 
-console.log(arr.length);
-console.log(arr.join(' '));
+console.log(len);
+console.log(answer);
